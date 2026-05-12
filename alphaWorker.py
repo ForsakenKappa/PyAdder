3567971cba95d6b018da96e62e6af34c7247dcd2
ForsakenKappa <75193955+ForsakenKappa@users.noreply.github.com>
2026-05-12 21:11:20 +0300

    Added parsing

--- alphaWorker.py
+++ alphaWorker.py
@@ -14,10 +14,11 @@ if __name__ == "alphaWorker":
 
         def appendCharset(self, charset):
             #Charset выглядит как массив из двух элементов, например ["чупапи","муняню"]
+            parsedCharset = self.parseCharset(charset)
             for row in range(1, 9999):
                 DBrow = self._activeWS[row]
                 if len(DBrow) == 0:
-                    self._activeWS.append(charset)
+                    self._activeWS.append(parsedCharset)
 
         def getCharset(self, row):
             charset = []
@@ -27,5 +28,11 @@ if __name__ == "alphaWorker":
 
         def deleteRow(self, row):
             self._activeWS.delete_rows(row)
+
+        def parseCharset(self, charsetStr = ""):
+            returnArr = charsetStr.split(";")
+            if len(returnArr) != 2:
+                raise ValueError
+            return returnArr
 
     pass
