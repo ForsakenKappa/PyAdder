dc3219727ffebbfcde4a99e54e3168fb368c1569
ForsakenKappa <75193955+ForsakenKappa@users.noreply.github.com>
2026-05-12 21:04:01 +0300

    Alpha worker minimum

--- alphaWorker.py
+++ alphaWorker.py
@@ -3,8 +3,6 @@ if __name__ == "__main__":
 
 if __name__ == "alphaWorker":
 
-
-
     import openpyxl as pyxl
     from openpyxl import Workbook
 
@@ -12,9 +10,22 @@ if __name__ == "alphaWorker":
 
         def __init__(self):
             self._dataBase = pyxl.load_workbook("./resources/dataBase.xlsx")
-        def populate(self, charset):
+            self._activeWS = self._dataBase.active
+
+        def appendCharset(self, charset):
+            #Charset выглядит как массив из двух элементов, например ["чупапи","муняню"]
             for row in range(1, 9999):
-                DBrow = self._dataBase[row]
-    
+                DBrow = self._activeWS[row]
+                if len(DBrow) == 0:
+                    self._activeWS.append(charset)
+
+        def getCharset(self, row):
+            charset = []
+            DBrow = self._activeWS[row]
+            for rowv in DBrow:
+                charset.append(rowv.value)
+
+        def deleteRow(self, row):
+            self._activeWS.delete_rows(row)
 
     pass
